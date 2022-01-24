1ed5269c8cad3b404e70cab7777f060fb2719085
Laura Jaime <laura.jv@coditramuntana.com>
2022-01-24 13:36:02 +0100

    Add tags in assemblies for challenge host

---
+++ app/decorators/cells/decidim/assemblies/assembly_m_cell_decorator.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+Decidim::Assemblies::AssemblyMCell.class_eval do
+
+  alias_method :original_statuses, :statuses
+
+  private
+
+  def statuses
+    if current_organization.host == "participa.challenge.cat"
+      [:open_close, :follow]
+    else
+      original_statuses
+    end
+  end
+
+  def open_close_status
+    closing_date = model.closing_date
+
+    if closing_date > Date.today
+      content_tag(:p, t(".open"), class: 'tag open')
+    else
+      content_tag(:p, t(".close"), class: 'tag close')
+    end
+  end
+end
