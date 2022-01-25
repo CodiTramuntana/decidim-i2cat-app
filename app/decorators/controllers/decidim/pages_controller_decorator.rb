12b83d93e536d62802852123c190842c4c3a1c3a
Laura Jaime <laura.jv@coditramuntana.com>
2022-01-25 08:31:52 +0100

    Remove override and refactor decorator

--- app/decorators/controllers/decidim/pages_controller_decorator.rb
+++ app/decorators/controllers/decidim/pages_controller_decorator.rb
@@ -2,11 +2,18 @@ Decidim::PagesController.class_eval do
   alias_method :original_index, :index
 
   def index
+    enforce_permission_to :read, :public_page
+
     if current_organization.host == "reptes.dca.cat"
       @topic = Decidim::StaticPageTopic.find_by("title->>'ca' = 'La DCA'")
       @page = current_organization.static_pages.first
       enforce_permission_to :read, :public_page, page: @page
       @pages = @topic&.pages
+
+      render template: "decidim/pages/who_we_are"
+    elsif current_organization.host == "participa.challenge.cat"
+      @page = Decidim::StaticPage.find_by(slug: "smartcataloniachallenge")
+      redirect_to page_path(@page || StaticPage.take)
     else
       original_index
     end
