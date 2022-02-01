0cee3cb58014bf9f5bcd23a7202cdaa209a2ef97
Laura Jaime <laura.jv@coditramuntana.com>
2022-02-01 15:54:42 +0100

    Set tags in all hosts

--- config/initializers/i2cat.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module I2cat
-  HOST_W_CUSTOM_TAGS_ASSEMBLIES = "participa.challenge.cat"
-end
--- spec/system/assemblies_spec.rb
+++ spec/system/assemblies_spec.rb
@@ -8,7 +8,6 @@ describe "Visit assemblies", type: :system do
 
   context "when visiting the assemblies participatory space with tags" do
     before do
-      stub_const("I2cat::HOST_W_CUSTOM_TAGS_ASSEMBLIES", organization.host)
       switch_to_host(organization.host)
       visit decidim_assemblies.assemblies_path
     end
@@ -21,24 +20,6 @@ describe "Visit assemblies", type: :system do
 
         expect(page).to have_selector(".open_close_status")
         expect(page).not_to have_selector(".creation_date_status")
-      end
-    end
-  end
-
-  context "when visiting the assemblies participatory space without tags" do
-    before do
-      switch_to_host(organization.host)
-      visit decidim_assemblies.assemblies_path
-    end
-
-    it "lists the parent assemblies" do
-      within "#parent-assemblies" do
-        within "#parent-assemblies h3" do
-          expect(page).to have_content("1")
-        end
-
-        expect(page).not_to have_selector(".open_close_status")
-        expect(page).to have_selector(".creation_date_status")
       end
     end
   end
