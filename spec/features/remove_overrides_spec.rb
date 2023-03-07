1292eb6b21d61e50e0cf376d62819bb42c7c444a
Laura Jaime <laura.jv@coditramuntana.com>
2023-03-07 16:42:41 +0100

    Remove unused translations and override test

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -3,8 +3,4 @@
 require "rails_helper"
 
 RSpec.describe "Overrides" do
-  it "remove overrides after upgrading to v0.25" do
-    # - Remove :ca and :es transations about whitelist errors
-    expect(Decidim.version).to be < "0.25"
-  end
 end
