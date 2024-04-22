4cb44295c1fa0ed23c044d7be1aa979fe3f832ff
Laura Jaime <laura.jv@coditramuntana.com>
2024-04-22 08:19:18 +0200

    Change host by id

--- app/overrides/layouts/decidim/_application.rb
+++ app/overrides/layouts/decidim/_application.rb
@@ -3,5 +3,5 @@
 Deface::Override.new(virtual_path: +"layouts/decidim/_application",
                      name: "set organization host in data attributes",
                      add_to_attributes: "body",
-                     attributes: { "data-host": "<%= current_organization.host %>" },
+                     attributes: { "data-organization-id": "<%= current_organization.id %>" },
                      original: "41930cfb2025a54579e900288234b4b6e3e8aa04")
