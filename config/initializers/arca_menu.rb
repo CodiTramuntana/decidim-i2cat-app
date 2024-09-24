5c2c1bb2b233136755aeead90398e82ab5a81e43
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-09-24 15:20:43 +0200

    Customize main menu for ARCA (#138)
    * Customize main menu for ARCA
    
    * Add tests and remove redundant file rails_helper
    
    * Remove useless commented code
    
    * Apply translations to ARCA's main menu
    
    * Fix :es translation

---
+++ config/initializers/arca_menu.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Decidim.menu :menu do |menu|
+  if current_organization.id.to_s == ENV.fetch("ARCA_ORGANIZATION_ID", nil)
+    menu.remove_item :participatory_processes
+    menu.remove_item :assemblies
+    menu.remove_item :conferences
+    menu.add_item :agenda_rural,
+                  I18n.t("arca_menu.agenda_rural"),
+                  "https://www.donadelasegarraalurgellunclavell.cat/assemblies/reptes",
+                  position: 2.01
+    menu.add_item :agenda_rural,
+                  I18n.t("arca_menu.atlas_mon_rural"),
+                  "https://www.donadelasegarraalurgellunclavell.cat/assemblies/atles",
+                  position: 2.02
+  end
+end
--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -13,24 +13,39 @@ describe "Homepage", type: :system do
       available_locales: [:ca, :en, :es]
     )
   end
+  let(:arca_organization_id) { nil }
   let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_ca"=>"Benvinguda a Decidim Application" } }
   let!(:sub_hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero }
 
   before do
+    ENV["ARCA_ORGANIZATION_ID"]= arca_organization_id
     switch_to_host(organization.host)
     visit decidim.root_path(locale: I18n.locale)
   end
 
   it "loads and shows organization name and main blocks" do
-    visit decidim.root_path
+    within ".navbar" do
+      # not ARCA
+      expect(page).to have_content("Home\nHelp")
+    end
 
     expect(page).to have_content("Decidim Application")
     within "section.hero .hero__container" do
       expect(page).to have_content("Benvinguda a Decidim Application")
     end
-    within "section.subhero" do
+    within ".footer-separator section.subhero" do
       subhero_msg= translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1").gsub(%r{<script>(((?!</script>).)*)</script>}mi, "\\1")
       expect(page).to have_content(subhero_msg)
+    end
+  end
+
+  context "when organization is ARCA" do
+    let(:arca_organization_id) { organization.id.to_s }
+
+    it "renders its custom menu items" do
+      within ".navbar" do
+        expect(page).to have_content("Home\nCatalonia's Rural Agenda\nRural World Atlas\nHelp")
+      end
     end
   end
 end
