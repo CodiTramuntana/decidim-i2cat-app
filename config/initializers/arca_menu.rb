112e200533bc07647cb726d5757a1a747362d313
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-09-24 14:33:26 +0200

    Remove useless commented code

--- config/initializers/arca_menu.rb
+++ config/initializers/arca_menu.rb
@@ -14,13 +14,4 @@ Decidim.menu :menu do |menu|
                   "https://www.donadelasegarraalurgellunclavell.cat/assemblies/atles",
                   position: 2.02
   end
-  # menu.add_item :assemblies,
-  #               I18n.t("menu.assemblies", scope: "decidim.admin"),
-  #               decidim_admin_assemblies.assemblies_path,
-  #               icon_name: "dial",
-  #               position: 2.2,
-  #               active: is_active_link?(decidim_admin_assemblies.assemblies_path) ||
-  #                       is_active_link?(decidim_admin_assemblies.assemblies_types_path) ||
-  #                       is_active_link?(decidim_admin_assemblies.edit_assemblies_settings_path),
-  #               if: allowed_to?(:enter, :space_area, space_name: :assemblies)
 end
