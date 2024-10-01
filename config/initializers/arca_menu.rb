ffb6653daef302727db85ed0a61ed725d54e647b
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-10-01 14:54:35 +0200

    Fix arca menu name for mon_rural

--- config/initializers/arca_menu.rb
+++ config/initializers/arca_menu.rb
@@ -9,7 +9,7 @@ Decidim.menu :menu do |menu|
                   I18n.t("arca_menu.agenda_rural"),
                   "https://www.donadelasegarraalurgellunclavell.cat/assemblies/reptes",
                   position: 2.01
-    menu.add_item :agenda_rural,
+    menu.add_item :mon_rural,
                   I18n.t("arca_menu.atlas_mon_rural"),
                   "https://www.donadelasegarraalurgellunclavell.cat/assemblies/atles",
                   position: 2.02
