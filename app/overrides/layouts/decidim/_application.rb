2b026b772e8d3077e9c4bbca930232d8acb79c33
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-11-06 14:13:25 +0100

    Fix deface rendering at body level (#144)
    * Remove test/ unused dir
    
    * Remove unused config/initializers/devise.rb
    
    * Fix adding of data-organization-id attribute
    
    * Rubocopify

--- app/overrides/layouts/decidim/_application.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(virtual_path: +"layouts/decidim/_application",
-                     name: "set organization host in data attributes",
-                     add_to_attributes: "body",
-                     attributes: { "data-organization-id": "<%= current_organization.id %>" },
-                     original: "41930cfb2025a54579e900288234b4b6e3e8aa04")
---
+++ app/overrides/layouts/decidim/_wrapper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Deface::Override.new(virtual_path: +"layouts/decidim/_wrapper",
+                     name: "Set organization id in wrapper div data attributes",
+                     insert_before: "div.off-canvas-wrapper",
+                     text: '<div data-organization-id="<%= current_organization.id %>" />',
+                     original: "e9c7043ff235d7f0cdce66a0445cafca5e4eec81")
